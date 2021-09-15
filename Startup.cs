9f18df95889e59bbd831c0e05a0aae543b10f7b9
daiho hayashi <daiho_hayashi@nifty.com>
2021-09-15 16:26:47 +0900

    scaffolding RPMovie

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace RPMovie
 {
@@ -24,6 +25,9 @@ namespace RPMovie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<RPMovieContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("RPMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
